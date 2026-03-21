61f76b2de6a5c89ee5e60c9d07447772a34e68f4
Michael Vega <michaelvega.noirsys@gmail.com>
2026-03-21 10:20:48 -0400

    Fix fence sites blank deploy (relative Vite base)

---
+++ websites/ashtabula-fence/src/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import SnapToQuoteFence from './components/SnapToQuoteFence';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex flex-col font-sans">
+      <h1 style={{position: "absolute", left: "-9999px"}}>Ashtabula Fence Co.</h1>
+
+      <main className="flex-1 p-4 lg:p-6 overflow-hidden">
+        <div className="max-w-7xl mx-auto h-full min-h-[calc(100vh-2rem)]">
+          <SnapToQuoteFence />
+        </div>
+      </main>
+    </div>
+  );
+}
